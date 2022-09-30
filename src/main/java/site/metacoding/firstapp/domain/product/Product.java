706a2ba9dc325ca6b6babce35171a596c70ecedc
shyue02 <shyue02@naver.com>
2022-09-30 11:18:53 +0900

    ProductDao Junit 테스트 완료

--- src/main/java/site/metacoding/firstapp/domain/product/Product.java
+++ src/main/java/site/metacoding/firstapp/domain/product/Product.java
@@ -10,7 +10,30 @@ import lombok.Setter;
 public class Product {
     private Integer productId;
     private String productName;
-    private Integer ProductPrice;
-    private Integer product_Qty;
+    private Integer productPrice;
+    private Integer productQty;
     private Timestamp createdAt;
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+
+    }
+
+    public Product() {
+
+    } // MyBatis에게 필요한
+
+    // 생성자초기화 할 때 사용
+    public Product(String productName, Integer productPrice, Integer productQty) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productQty = productQty;
+    }
+
+    // update 할 때사용
+    public void update(Product product) {
+        this.productName = product.getProductName();
+        this.productPrice = product.getProductPrice();
+        this.productQty = product.getProductQty();
+    }
 }
